1dd22b3d0a4236e486a678abc92e702fb196b73d
bhspppppp <beach.p211@gmail.com>
2026-07-17 02:43:50 +0700

    final commit

---
+++ tree.h
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef Binary_Search_Tree
+#define Binary_Search_Tree
+
+template<class T> class BST;
+
+template<class T>
+class Node {
+public:
+	Node() { left = right = NULL; }
+	Node(const T& el, int ln, Node *l = 0, Node *r = 0) {
+		key = el; left = l; right = r;
+		lines.push_back(ln);
+	}
+	T key;
+	vector<int> lines;
+	Node *left, *right;
+};
+
+template<class T>
+class BST {
+public:
+	BST() { root = 0; }
+	~BST() { clear(); }
+	void clear() { clear(root); root = 0; }
+	bool isEmpty() { return root == 0; }
+	void inorder() { inorder(root); }
+	void insert(const T& el, int ln);
+	void findAndDelete(const T& el);
+	void deleteNode(Node<T> *& node);
+
+protected:
+	Node<T> *root;
+
+	void clear(Node<T> *p);
+	void inorder(Node<T> *p);
+
+};
+
+template<class T>
+void BST<T>::clear(Node<T> *p)
+{
+	if (p != 0) {
+		clear(p->left);
+		clear(p->right);
+		delete p;
+	}
+}
+
+template<class T>
+void BST<T>::inorder(Node<T> *p) {
+	if (p != 0) {
+		inorder(p->left);
+		cout << p->key << "  ";
+		for (int i = 0; i < p->lines.size(); i++) {
+			if (i > 0) cout << ", ";
+			cout << p->lines[i];
+		}
+		cout << endl;
+		inorder(p->right);
+	}
+}
+
+template<class T>
+void BST<T>::insert(const T &el, int ln) {
+	Node<T> *p = root, *prev = 0;
+	while (p != 0) {
+		prev = p;
+		if (el == p->key) {
+			if (p->lines.empty() || p->lines.back() != ln)
+				p->lines.push_back(ln);
+			return;
+		}
+		if (p->key < el)
+			p = p->right;
+		else
+			p = p->left;
+	}
+	if (root == 0)
+		root = new Node<T>(el, ln);
+	else if (prev->key < el)
+		prev->right = new Node<T>(el, ln);
+	else
+		prev->left = new Node<T>(el, ln);
+}
+
+template<class T>
+void BST<T>::findAndDelete(const T& el) {
+	Node<T> *node = root, *prev = 0;
+	while (node != 0) {
+		if (node->key == el)
+			break;
+		prev = node;
+		if (node->key < el)
+			node = node->right;
+		else node = node->left;
+	}
+	if (node != 0 && node->key == el)
+		if (node == root)
+			deleteNode(root);
+		else if (prev->left == node)
+			deleteNode(prev->left);
+		else deleteNode(prev->right);
+}
+
+template<class T>
+void BST<T>::deleteNode(Node<T> *&node) {
+	Node<T> *prev, *tmp = node;
+	if (node->right == 0)
+		node = node->left;
+	else if (node->left == 0)
+		node = node->right;
+	else {
+		tmp = node->left;
+		prev = node;
+		while (tmp->right != 0) {
+			prev = tmp;
+			tmp = tmp->right;
+		}
+		node->key = tmp->key;
+		if (prev == node)
+			prev->left = tmp->left;
+		else prev->right = tmp->left;
+	}
+	delete tmp;
+}
+
+#endif
